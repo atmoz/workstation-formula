571aff95379bd556701bbba1ba19bf44f0b714b8
Adrian Dvergsdal <github.com@konto.atmoz.no>
2018-02-27 11:47:01 +0100

    Working dotfiles-init.sh

--- states/system/dotfiles-init.sh
+++ states/system/dotfiles-init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Tracking dotfiles using a bare git repo
+# https://news.ycombinator.com/item?id=11071754
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+# https://github.com/Siilwyn/my-dotfiles/tree/master/.my-dotfiles
 set -eu
 
 # Must clone with https, because salt-call is run with root
@@ -10,16 +14,15 @@ bin_repo='git@github.com:atmoz/bin.git'
 dotfiles_repo_pub='https://github.com/atmoz/dotfiles.git'
 dotfiles_repo='git@github.com:atmoz/dotfiles.git'
 
-(
-    git clone $bin_repo_pub bin
-    cd bin
-    git remote set-url origin $bin_repo
-)
+if [ ! -d $HOME/bin ]; then
+    git clone $bin_repo_pub $HOME/bin
+    git -C $HOME/bin remote set-url origin $bin_repo
+fi
 
 git clone --bare $dotfiles_repo_pub $HOME/.dotfiles
-dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
-if ! $dotfiles checkout; then
+if ! $dotfiles checkout -q 2>/dev/null; then
     # Backup any exsisting files
     $dotfiles checkout 2>&1 \
         | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c \
@@ -27,4 +30,5 @@ if ! $dotfiles checkout; then
 fi
 
 $dotfiles checkout
+$dotfiles config --local status.showUntrackedFiles no
 $dotfiles remote set-url origin $dotfiles_repo
